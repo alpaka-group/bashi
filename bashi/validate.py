5cf7e4a2c67445bff4b79c832263703b1f1affc1
Simeon Ehrig <s.ehrig@hzdr.de>
2024-02-08 15:33:02 +0100

    implement validation tool to check if parameter-value-tuple passes the bashi filters

--- bashi/validate.py
+++ bashi/validate.py
@@ -1,8 +1,317 @@
-from bashi.generator import foo
+#!/usr/bin/env python3
+
+"""Command line tool to check filter rules."""
+
+import argparse
+from argparse import ArgumentParser, Namespace
+
+from typing import Sequence, Any, Callable, Optional, IO
+from collections import OrderedDict
+import io
+import sys
+from typeguard import typechecked
+import packaging.version as pkv
+
+from bashi.globals import *  # pylint: disable=wildcard-import,unused-wildcard-import
+from bashi.types import ParameterValue, ParameterValueTuple
+from bashi.versions import is_supported_version
+import bashi.filter_compiler_name
+import bashi.filter_compiler_version
+import bashi.filter_backend
+import bashi.filter_software_dependency
+
+
+@typechecked
+def cs(text: str, color: str) -> str:
+    """Prints colored text to the command line. The text printed after the function call has the
+    default color of the command line.
+
+    Args:
+        text (str): text to be colored
+        color (str): Name of the color. If color is unknown or empty use default color of the
+            command line.
+
+    Returns:
+        str: text with bash control symbols for coloring
+    """
+
+    output = ""
+    if color == "Red":
+        output += "\033[0;31m"
+    elif color == "Green":
+        output += "\033[0;32m"
+    elif color == "Yellow":
+        output += "\033[1;33m"
+    else:
+        return text
+
+    return output + text + "\033[0m"
+
+
+@typechecked
+def exit_error(text: str):
+    """Prints error message and exits application with error code 1.
+
+    Args:
+        text (str): Error message.
+    """
+    print(cs("ERROR: " + text, "Red"))
+    sys.exit(1)
+
+
+@typechecked
+class VersionCheck(argparse.Action):
+    """Verify that version can be parsed to package.version.Version.
+
+    Handles special values "ON" and "OFF", which will be parsed to 1.0.0 and 0.0.0
+    """
+
+    # check if argument has valid version shape
+    def __call__(
+        self,
+        parser: ArgumentParser,
+        namespace: Namespace,
+        values: str | Sequence[Any] | None,
+        option_string: str | None = None,
+    ):
+        if not values:
+            exit_error(f"argument {option_string}: expected one argument")
+
+        version: str = str(values)
+
+        if version == "OFF":
+            version = "0.0.0"
+
+        if version == "ON":
+            version = "1.0.0"
+
+        # use parse() function to validate that the version has a valid shape
+        try:
+            parsed_version = pkv.parse(version)
+            setattr(namespace, self.dest, parsed_version)
+        except packaging.version.InvalidVersion:
+            exit_error(f"Could not parse version of argument {option_string}: {version}")
+
+
+@typechecked
+class CompilerVersionCheck(argparse.Action):
+    """Tries to parse compiler versions string of the shape "name@version" to a parameter-value."""
+
+    # check if argument has valid version shape
+    def __call__(
+        self,
+        parser: ArgumentParser,
+        namespace: Namespace,
+        values: str | Sequence[Any] | None,
+        option_string: str | None = None,
+    ) -> None:
+        if not values:
+            exit_error(f"argument {option_string}: expected one argument")
+
+        parameter_value_str: str = str(values)
+
+        if "@" not in parameter_value_str:
+            exit_error(f"@ is missing in {option_string}={parameter_value_str}")
+            return
+
+        splitted_name_version = parameter_value_str.split("@", 1)
+        name = splitted_name_version[0]
+        version = splitted_name_version[1]
+
+        if name not in COMPILERS:
+            exit_error(f"Unknown compiler: {name}\nKnown compilers: {COMPILERS}")
+
+        # use parse() function to validate that the version has a valid shape
+        try:
+            parsed_version = pkv.parse(version)
+            setattr(namespace, self.dest, ParameterValue(name, parsed_version))
+        except packaging.version.InvalidVersion:
+            exit_error(f"Could not parse version number of {name}: {version}")
+
+
+def get_args() -> Namespace:
+    """Set up command line arguments and parsed it.
+
+    Returns:
+        Namespace: The parsed command line arguments
+    """
+    parser = argparse.ArgumentParser(description="Check if combination of parameters is valid.")
+
+    parser.add_argument(
+        "--host-compiler",
+        type=str,
+        action=CompilerVersionCheck,
+        help="Define host compiler. Shape needs to be name@version. " "For example gcc@10",
+    )
+
+    parser.add_argument(
+        "--device-compiler",
+        type=str,
+        action=CompilerVersionCheck,
+        help="Define device compiler. Shape needs to be name@version. " "For example nvcc@11.3",
+    )
+
+    for backend in BACKENDS:
+        if backend != ALPAKA_ACC_GPU_CUDA_ENABLE:
+            parser.add_argument(
+                "--" + backend,
+                type=str,
+                action=VersionCheck,
+                choices=["ON", "OFF"],
+                help=f"Set backend {backend} as enabled or disabled.",
+            )
+        else:
+            parser.add_argument(
+                "--" + backend,
+                type=str,
+                action=VersionCheck,
+                help=f"Set backend {backend} to disabled (OFF) or a specific CUDA SDK version.",
+            )
+
+    parser.add_argument("--ubuntu", type=str, action=VersionCheck, help="Ubuntu version.")
+
+    parser.add_argument("--cmake", type=str, action=VersionCheck, help="Set CMake version.")
+
+    parser.add_argument("--boost", type=str, action=VersionCheck, help="Set Boost version.")
+
+    parser.add_argument("--cxx", type=str, action=VersionCheck, help="C++ version.")
+
+    return parser.parse_args()
+
+
+@typechecked
+def check_single_filter(
+    filter_func: Callable[[ParameterValueTuple, Optional[IO[str]]], bool],
+    row: ParameterValueTuple,
+    required_parameter: List[str],
+) -> bool:
+    """Check if row passes a filter function.
+
+    Args:
+        filter_func (Callable[[ParameterValueTuple, Optional[IO[str]]], bool]): The filter function
+        row (ParameterValueTuple): row with parameter-value-tuples
+        required_parameter (List[str]): list of parameters, which will be used in the filter rule
+
+    Returns:
+        bool: True if the row passes the filter.
+    """
+    missing_parameters = ""
+
+    # get name of the filter for command line output.
+    filter_name: str = filter_func.__name__
+
+    # Each filter function has also type checked version ending with _typed.
+    # Remove _typed for better output.
+    if filter_name.endswith("_typechecked"):
+        filter_name = filter_name[: -len("_typechecked")]
+
+    # check if all required parameter are available
+    for req_name in required_parameter:
+        if req_name not in row:
+            missing_parameters += " " + req_name
+
+    if missing_parameters:
+        print(
+            cs(
+                f"skipped {filter_name}(), missing parameters ->" + missing_parameters,
+                "Yellow",
+            )
+        )
+        return False
+
+    msg = io.StringIO()
+
+    if filter_func(row, msg):
+        print(cs(f"{filter_name}() returns True", "Green"))
+        return True
+    else:
+        print(cs(f"{filter_name}() returns False", "Red"))
+        if msg.getvalue() != "":
+            print("  " + msg.getvalue())
+        return False
+
+
+@typechecked
+def check_filter_chain(row: ParameterValueTuple) -> bool:
+    """Test a row with the bashi default filter chain.
+
+    Args:
+        row (ParameterValueTuple): row to test
+
+    Returns:
+        bool: True if row passes all filters
+    """
+    all_true = 0
+    all_true += int(
+        check_single_filter(
+            bashi.filter_compiler_name.compiler_name_filter_typechecked,
+            row,
+            bashi.filter_compiler_name.get_required_parameters(),
+        )
+    )
+
+    all_true += int(
+        check_single_filter(
+            bashi.filter_compiler_version.compiler_version_filter_typechecked,
+            row,
+            bashi.filter_compiler_version.get_required_parameters(),
+        )
+    )
+    all_true += int(
+        check_single_filter(
+            bashi.filter_backend.backend_filter_typechecked,
+            row,
+            bashi.filter_backend.get_required_parameters(),
+        )
+    )
+    all_true += int(
+        check_single_filter(
+            bashi.filter_software_dependency.software_dependency_filter_typechecked,
+            row,
+            bashi.filter_software_dependency.get_required_parameters(),
+        )
+    )
+
+    # each filter add a one, if it was successful
+    return all_true == 4
 
 
 def main():
-    foo()
+    """Entry point for the application."""
+    args = get_args()
+
+    row: ParameterValueTuple = OrderedDict()
+
+    for param, arg in [
+        (HOST_COMPILER, "host_compiler"),
+        (DEVICE_COMPILER, "device_compiler"),
+    ]:
+        if hasattr(args, arg) and getattr(args, arg) is not None:
+            row[param] = getattr(args, arg)
+
+    for backend in BACKENDS:
+        if hasattr(args, backend) and getattr(args, backend) is not None:
+            row[backend] = ParameterValue(backend, getattr(args, backend))
+
+    for param, arg in [
+        (UBUNTU, "ubuntu"),
+        (CMAKE, "cmake"),
+        (BOOST, "boost"),
+        (CXX_STANDARD, "cxx"),
+    ]:
+        if hasattr(args, arg) and getattr(args, arg) is not None:
+            row[param] = ParameterValue(param, getattr(args, arg))
+
+    for val_name, val_version in row.values():
+        if not is_supported_version(val_name, val_version):
+            print(
+                cs(
+                    f"WARNING: {val_name} {val_version} is not officially supported.",
+                    "Yellow",
+                )
+            )
+
+    sys.exit(int(not check_filter_chain(row)))
 
 
 if __name__ == "__main__":
---
+++ tests/test_supported_version.py
@@ -0,0 +1,103 @@
+# pylint: disable=missing-docstring
+import unittest
+from typing import List, Union
+import packaging.version as pkv
+from bashi.versions import VERSIONS, is_supported_version
+from bashi.globals import *  # pylint: disable=wildcard-import,unused-wildcard-import
+
+
+def parse_to_version(input_list: List[Union[int, float, str]]) -> List[pkv.Version]:
+    parsed_versions: List[pkv.Version] = []
+
+    for v in input_list:
+        parsed_versions.append(pkv.parse(str(v)))
+
+    return parsed_versions
+
+
+class TestParameterValueGenerator(unittest.TestCase):
+    def test_compiler_versions(self):
+        # test if version is supported for the following tests
+        for name, version in [(GCC, 12), (CLANG, 14), (NVCC, 12.1)]:
+            self.assertTrue(
+                pkv.parse(str(version)) in parse_to_version(VERSIONS[name]),
+                f"{name} {version} is not supported by bashi",
+            )
+
+        # test if version is unsupported for the following tests
+        for name, version in [(GCC, 1), (NVCC, 4.7)]:
+            self.assertFalse(
+                pkv.parse(str(version)) in parse_to_version(VERSIONS[name]),
+                f"{name} {version} is supported by bashi",
+            )
+
+        self.assertTrue(is_supported_version(GCC, pkv.parse(str(12))))
+        self.assertTrue(is_supported_version(CLANG, pkv.parse(str(14))))
+        self.assertTrue(is_supported_version(CLANG_CUDA, pkv.parse(str(14))))
+        self.assertTrue(is_supported_version(NVCC, pkv.parse(str(12.1))))
+
+        self.assertFalse(is_supported_version(GCC, pkv.parse(str(1))))
+        self.assertFalse(is_supported_version(NVCC, pkv.parse(str(4.7))))
+
+        # unknown compiler should throw an error
+        self.assertRaises(
+            ValueError, is_supported_version, "fancy-cpp-compiler", pkv.parse(str(12))
+        )
+
+    def test_backend_versions(self):
+        # all compilers except the CUDA backend has only the versions ON and OFF
+        for backend in BACKENDS:
+            if backend != ALPAKA_ACC_GPU_CUDA_ENABLE:
+                self.assertTrue(is_supported_version(backend, ON_VER))
+                self.assertTrue(is_supported_version(backend, OFF_VER))
+                self.assertFalse(is_supported_version(backend, pkv.parse(str(12.1))))
+
+        # test if version is supported for the following tests
+        for name, version in [(NVCC, 12.1)]:
+            self.assertTrue(
+                pkv.parse(str(version)) in parse_to_version(VERSIONS[name]),
+                f"{name} {version} is not supported by bashi",
+            )
+
+        # test if version is unsupported for the following tests
+        for name, version in [(NVCC, ON_VER), (NVCC, 4.7)]:
+            self.assertFalse(
+                pkv.parse(str(version)) in parse_to_version(VERSIONS[name]),
+                f"{name} {version} is supported by bashi",
+            )
+
+        self.assertTrue(is_supported_version(ALPAKA_ACC_GPU_CUDA_ENABLE, OFF_VER))
+        self.assertFalse(is_supported_version(ALPAKA_ACC_GPU_CUDA_ENABLE, ON_VER))
+        self.assertTrue(is_supported_version(ALPAKA_ACC_GPU_CUDA_ENABLE, pkv.parse(str(12.1))))
+
+        # unknown backend should throw an error
+        self.assertRaises(ValueError, is_supported_version, "alpaka_Backend_esoteric_acc", OFF_VER)
+
+    def test_software_versions(self):
+        for name, version in [
+            (CMAKE, 3.18),
+            (CMAKE, 3.21),
+            (BOOST, "1.80.0"),
+            (UBUNTU, "20.04"),
+            (CXX_STANDARD, 17),
+        ]:
+            # test if version is supported for the following tests
+            self.assertTrue(
+                pkv.parse(str(version)) in parse_to_version(VERSIONS[name]),
+                f"{name} {version} is not supported by bashi",
+            )
+            self.assertTrue(is_supported_version(name, pkv.parse(str(version))))
+
+        for name, version in [
+            (CMAKE, 2.78),
+            (CMAKE, 4546.546),
+            (BOOST, 0.93),
+            (UBUNTU, 10.05),
+            (CXX_STANDARD, 5),
+        ]:
+            # test if version is unsupported for the following tests
+            self.assertFalse(
+                pkv.parse(str(version)) in parse_to_version(VERSIONS[name]),
+                f"{name} {version} is supported by bashi",
+            )
+            self.assertFalse(is_supported_version(name, pkv.parse(str(version))))
